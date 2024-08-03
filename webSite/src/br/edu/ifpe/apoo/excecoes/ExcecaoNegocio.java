856cf420782d3aca251245d6ea0f0c1a7f6235c9
nadhon <70290966+nadhon@users.noreply.github.com>
2024-08-03 09:26:29 -0300

    refazendo as entidades

---
+++ webSite/src/br/edu/ifpe/apoo/excecoes/ExcecaoNegocio.java
@@ -0,0 +1,9 @@
+package br.edu.ifpe.apoo.excecoes;
+
+@SuppressWarnings("serial")
+public class ExcecaoNegocio extends Exception {
+	public ExcecaoNegocio(String message) {
+		super (message);
+	}
+
+}
